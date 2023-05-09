6cac63183eaed83bf11ccb38e04f7896ccc0a613
philljohnson <131991625+philljohnson@users.noreply.github.com>
2023-05-09 11:30:58 -0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const name = "My Token";
-  const symbol = "MTK";
-  const minPurchase = hre.ethers.utils.parseEther("0.01");
-  const maxPurchase = hre.ethers.utils.parseEther("0.05");
-  const rate = 1000;
-  const softCap = hre.ethers.utils.parseEther("0.1");
-  const hardCap = hre.ethers.utils.parseEther("1");
-  const startTime = Math.floor(new Date("2023-05-08") / 1000); // Start in 1 minute
-  const endTime = Math.floor(new Date("2023-05-10") / 1000); // End in 1 day
 
   const ICO = await hre.ethers.getContractFactory("ICO");
-  const ico = await ICO.deploy(
-    name,
-    symbol,
-    minPurchase,
-    maxPurchase,
-    rate,
-    softCap,
-    hardCap,
-    startTime,
-    endTime
-  );
-
+  const ico = await ICO.deploy();
   await ico.deployed();
   console.log("ICO deployed to:", ico.address);
 }
