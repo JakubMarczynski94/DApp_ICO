f1dd7b4a99ec56fc9ff3daeb76899f9c363992e7
philljohnson <philljohnson739@gmail.com>
2023-05-10 03:27:42 -0700

    Update contract.

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,110 +1,62 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract ICO is ERC20, Ownable {
-    uint256 public minPurchase;
-    uint256 public maxPurchase;
-    uint256 public rate;
-    uint256 public softCap;
-    uint256 public hardCap;
-    uint256 public startTime;
-    uint256 public endTime;
-    uint256 public depositAmount;
-    bool public active;
+    address public _owner;
+    uint256 public minPurchase = 0.01*10**18;
+    uint256 public maxPurchase = 0.05*10**18;
+    uint256 public rate = 1000;
+    uint256 public tokenNumber = 500000;
+    uint256 public softCap = 0.1*10**18;
+    uint256 public hardCap = 10**18;
+    uint256 public startTime = 1683586800;
+    uint256 public endTime = 1683745200;
+    uint256 public depositAmount = 0;
 
-    enum State {
-        BEFORE,
-        RUNNING,
-        END_FAIL,
-        END_SUCCESS,
-        HALTED
-    }
-    State public ICOState;
+    mapping(address => uint) public _deposit;
 
     constructor(
-        string memory name,
-        string memory symbol,
-        uint256 _minPurchase,
-        uint256 _maxPurchase,
-        uint256 _rate,
-        uint256 _softCap,
-        uint256 _hardCap,
-        uint256 _startTime,
-        uint256 _endTime
-    ) ERC20(name, symbol) {
-        minPurchase = _minPurchase;
-        maxPurchase = _maxPurchase;
-        rate = _rate;
-        softCap = _softCap;
-        hardCap = _hardCap;
-        startTime = _startTime;
-        endTime = _endTime;
-        ICOState = State.RUNNING;
-        transferOwnership(msg.sender);
+    ) ERC20("LKS-Token", "LKS") {
+        _owner = msg.sender;
+        // transferOwnership(msg.sender);
+        _mint(_owner, tokenNumber);
     }
 
     function deposit() external payable {
-        require(ICOState == State.RUNNING, "ICO not active");
         require(block.timestamp >= startTime, "ICO has not started yet");
         require(block.timestamp <= endTime, "ICO has ended");
+        console.log(msg.value);
 
-        uint256 amount = msg.value * rate;
-        require(amount >= minPurchase, "Amount is below minimum purchase limit");
-        require(amount <= maxPurchase, "Amount is above maximum purchase limit");
-
-        _mint(msg.sender, amount);
+        uint256 amount = msg.value / rate;
+        require(amount + _deposit[msg.sender] >= minPurchase, "Amount is below minimum purchase limit");
+        require(amount + _deposit[msg.sender] <= maxPurchase, "Amount is above maximum purchase limit");
+        require(amount + depositAmount < hardCap, "depositAmount over hardCap");
         depositAmount += amount;
-
-        if(block.timestamp > endTime && depositAmount < softCap){
-            ICOState = State.END_FAIL;
-        }
-        if(block.timestamp > endTime && depositAmount < softCap){
-            ICOState = State.END_SUCCESS;
-        }
-        if(depositAmount > hardCap) {
-            ICOState = State.END_SUCCESS;
-        }
-    }
-
-    function stopICO() external onlyOwner {
-        ICOState = State.HALTED;
+        _deposit[msg.sender] += amount;
     }
 
     function withdraw() external {
-        require(ICOState == State.END_SUCCESS, "Cannot withdraw while ICO is active");
+        require(block.timestamp > endTime, "Date is not reached");
+        require(depositAmount < softCap, "Soft cap not reached");
+        require(_deposit[msg.sender] > 0, "user has no deposit");
 
-        uint256 balance = address(this).balance;
-        payable(owner()).transfer(balance);
+        uint256 amount = _deposit[msg.sender];
+        depositAmount -= amount;
+        _deposit[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
     }
 
     function claim() external {
-        require(ICOState == State.END_FAIL, "Cannot claim while ICO is active");
+        require(depositAmount > softCap, "softCap not reached");
+        require(_deposit[msg.sender] > 0, "don't have deposit");
+        require(block.timestamp >= endTime || depositAmount >= hardCap, "Success condition not reached");
 
-        uint256 amount = balanceOf(msg.sender);
-        require(amount > 0, "No tokens to claim");
-
-        _burn(msg.sender, amount);
-        payable(msg.sender).transfer(amount / rate);
-    }
-
-    function getICOState() external view returns (string memory) {
-        if (ICOState == State.BEFORE) {
-            return "Not Started";
-        } else if (ICOState == State.RUNNING) {
-            return "Running";
-        } else if (ICOState == State.END_FAIL) {
-            return "Failed End";
-        } else if (ICOState == State.END_SUCCESS) {
-            return "Success End";
-        }        else {
-            return "Halted";
-        }
-    }
-
-    function getICODate() external view returns (uint256){
-        return startTime;
+        uint256 amount = _deposit[msg.sender] * rate;
+        payable(_owner).transfer(_deposit[msg.sender]);
+        _deposit[msg.sender] = 0;
+        transferFrom(_owner, msg.sender, amount);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const name = "My Token";
-  const symbol = "MTK";
-  const minPurchase = hre.ethers.utils.parseEther("0.1");
-  const maxPurchase = hre.ethers.utils.parseEther("10");
-  const rate = 1000;
-  const softCap = hre.ethers.utils.parseEther("1000");
-  const hardCap = hre.ethers.utils.parseEther("10000");
-  const startTime = Math.floor(Date.now() / 1000) + 60; // Start in 1 minute
-  const endTime = Math.floor(Date.now() / 1000) + 86400; // End in 1 day
 
   const ICO = await hre.ethers.getContractFactory("ICO");
-  const ico = await ICO.deploy(
-    name,
-    symbol,
-    minPurchase,
-    maxPurchase,
-    rate,
-    softCap,
-    hardCap,
-    startTime,
-    endTime
-  );
-
+  const ico = await ICO.deploy();
   await ico.deployed();
   console.log("ICO deployed to:", ico.address);
 }
